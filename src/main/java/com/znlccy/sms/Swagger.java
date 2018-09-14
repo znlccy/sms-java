bd6781c68b9861d06618525b43f04b8e6c07fab5
znlccy <756311868@qq.com>
2018-09-14 19:35:31 +0800

    提交readme文件

---
+++ src/main/java/com/znlccy/sms/Swagger.java
@@ -0,0 +1,11 @@
+package com.znlccy.sms;
+
+/**
+ * @Author: Administrator
+ * @Datetime: 2018/9/14-19:32
+ * @Version: v1.0.0
+ * @Comment: Swagger配置文件
+ */
+public class Swagger {
+
+}
---
+++ src/main/java/com/znlccy/sms/config/MongoConfig.java
@@ -0,0 +1,15 @@
+package com.znlccy.sms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Administrator
+ * @Datetime: 2018/9/14-19:32
+ * @Version: v1.0.0
+ * @Comment: MongoDB配置文件
+ */
+
+@Configuration
+public class MongoConfig {
+
+}
